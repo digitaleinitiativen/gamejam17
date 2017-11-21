0ccdee0ec85f91414f3a97f0c5c7e4c6aab50db0
joemoe <johannes@crate.io>
2017-11-21 22:55:07 +0100

    some improvements

--- game/game.js
+++ game/game.js
@@ -5,7 +5,24 @@ var SPAWN_RATE = 3000;
 var SPAWN_REDUX = 50;
 var SPAWN_RAND = 400;
 var ASSET_VERSION = 1; //(new Date()).getTime();
-var BASE_PATH = 'game/'
+var BASE_PATH = 'game/';
+var YAY_WORDS = [
+    'YAY, A GAME BOY',
+    'YAMM',
+    'LUV U SANTA',
+    'WOW',
+    'AWESOME'
+];
+
+var NEJ_WORDS = [
+    "MAMA WON'T GET A PRESENT",
+    'LOST',
+    'DAMN',
+    'THAT WAS BAD',
+    'THROW IT AWAY DIEGO',
+    'WHAT WAS THAT',
+    'LOOOOOOOOL'
+];
 
 var state = {
     preload: function() {
@@ -47,7 +64,9 @@ var state = {
         this.player.body.gravity.y = GRAVITY;
         this.player.body.setSize(24, 36, 12, 0);
 
-        this.teaser = this.add.sprite(20, 20, 'teaser');
+        this.teaser = this.add.sprite(560, 20, 'teaser');
+
+        this.hints = this.add.group();
 
         this.scoreText = this.add.text(
             this.world.width / 2,
@@ -58,10 +77,31 @@ var state = {
                 align: 'center'
             }
         );
-        this.scoreText.backgroundColor = '#ffdd00';
         this.scoreText.anchor.setTo(0.5, 0.5);
         this.scoreText.fontSize = 20;
+
+        this.shareText = this.add.text(
+            this.world.width / 2,
+            this.world.height - this.world.height / 3,
+            "SHARE ON TWITTER",
+            {
+                fill: '#ffdd00',
+                align: 'center'
+            }
+        );
+        this.shareText.anchor.setTo(0.5, 0.5);
+        this.shareText.fontSize = 10;
+        this.shareText.inputEnabled = true;
+        this.shareText.events.onInputDown.add(function() {
+            var url = 'https://twitter.com/home?status=YAY,%20I%20scored%20' + this.score + '%20Points%20on%20the%20%40digitalInit%20%23GameJam17%20Game.%20%0Ahttp%3A//gamejam.diin.io';
+            window.open(url)
+        }, this);
+
+        this.shareText.visible = false;
+
+
         this.score = 0;
+
 
         this.upFree = true;
         this.spawnRate = SPAWN_RATE;
@@ -73,15 +113,22 @@ var state = {
         this.game.physics.arcade.collide(this.enemies, this.floor);
 
         if (this.gameStarted) {
+            obj = this;
             this.enemies.forEachAlive(function(enemy) {
                 if(enemy.body.x + enemy.body.width < this.game.world.bounds.left) {
                     enemy.kill();
                 }
             });
             this.presents.forEachAlive(function(present) {
-                if(present.body.y > this.game.world.height)
+                if(present.body.y > this.game.world.height) {
+                    obj.showHint(present, NEJ_WORDS[Math.floor(Math.random() * NEJ_WORDS.length)]);
                     present.kill();
-            })
+                    obj.addScore(-1000);
+                }
+            });
+
+            if(!this.gameOver) this.addScore(Math.round(this.time.physicsElapsed * 100));
+
         }
 
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP) || game.input.activePointer.leftButton.isDown) {
@@ -134,6 +181,7 @@ var state = {
         this.gameStarted = false;
         this.gameOver = false;
         this.score = 0;
+        this.shareText.visible = false;
         this.scoreText.setText("TOUCH TO\nSTART GAME");
         this.floor.reset(0, this.world.height - this.floor.body.height);
         this.player.reset(this.world.width / 4, this.floor.body.y - this.player.body.height);
@@ -180,9 +228,31 @@ var state = {
         enemy.body.velocity.y -= 60;
         enemy.animations.play('run-present');
         present.kill();
+        this.addScore(5000);
+        this.showHint(enemy, YAY_WORDS[Math.floor(Math.random() * YAY_WORDS.length)]);
+    },
+    addScore: function(addWhat) {
+        this.score += addWhat;
+        this.scoreText.setText("SCORE: " + this.score);  
+    },
+    showHint: function(focusOn, text) {
+        var hint = this.game.add.text(
+            focusOn.x,
+            focusOn.y,
+            text,
+            {
+                fill: '#ffdd00',
+                align: 'center'
+            }
+        );
+        hint.anchor.setTo(0.5, 0.5);
+        hint.fontSize = 20;
 
-        this.score += 50;
-        this.scoreText.setText("SCORE: " + this.score);
+        var move = this.game.add.tween(hint);
+        move.to({ y: hint.y - 100, x: hint.x - 100 * Math.random() + 50}, 1000);
+        move.onComplete.add(function() { hint.kill() }, this);
+        move.start();
+
     },
     setGameOver: function(player, enemy) {
         this.teaser.visible = true;
@@ -193,9 +263,11 @@ var state = {
             enemy.body.velocity.x = 0;
             enemy.animations.play('broken');
         });
+        this.showHint(enemy, 'CARAMBOOOOOOLAGEEEE');
 
         this.spawnTimer.stop();
         this.scoreText.setText("FINAL SCORE: " + this.score +"\nTOUCH TO TRY AGAIN");
+        this.shareText.visible = true;
         this.player.animations.play('broken');
         enemy.animations.play('broken');
     }
